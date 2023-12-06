e208535f5ea3947d3f5f5c0e256fb7f7c92a111a
sibelhmdv <78097592+sibelhmdv@users.noreply.github.com>
2023-12-06 05:47:49 +0400

    Update DatabaseConnection.java

--- bookstore/src/DatabaseConnection.java
+++ bookstore/src/DatabaseConnection.java
@@ -3,6 +3,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Scanner;
 
 public class DatabaseConnection {
     public static void main(String[] args) {
@@ -18,15 +19,18 @@ public class DatabaseConnection {
             // Establish the database connection
             Connection connection = DriverManager.getConnection(url, user, password);
 
-            insertOperationAuthors(connection);
-            insertOperationBooks(connection);
+
+            insertOperationAuthors(connection, 1);
+
+            insertOperationBooks(connection, 1);
+            
+            
 
             if (connection != null && !connection.isClosed()) {
                 System.out.println("Connected to the database!");
             } else {
                 System.out.println("Failed to connect to the database.");
             }
-            // Now you can perform database operations using the 'connection' object
 
             // Close the connection when done
             connection.close();
@@ -37,8 +41,16 @@ public class DatabaseConnection {
     }
 
     
-    private static void insertOperationBooks(Connection connection) {
+    private static void insertOperationBooks(Connection connection,int authorid) {
         try {
+
+            if (!authorExists(connection, authorid)) {
+                System.out.println("Author with ID " + authorid + " does not exist. Please insert the author first.");
+                return;
+            }
+
+            
+
             // Insert a new book
             String insertQuery = "INSERT INTO books (bookid, title, genre, authorid, stockquantity, price) " + 
             "VALUES (?,?,?,?,?,?)";
@@ -47,35 +59,57 @@ public class DatabaseConnection {
                 preparedStatement.setInt(1, 1);
                 preparedStatement.setString(2, "Beauty and the Beast"); 
                 preparedStatement.setString(3, "Romance");
-                preparedStatement.setInt(4, 1); 
+                preparedStatement.setInt(4, authorid); 
                 preparedStatement.setInt(5, 3);
                 preparedStatement.setDouble(6, 20.2);
 
                 int rowsInserted = preparedStatement.executeUpdate();
-                System.out.println(rowsInserted + " row(s) inserted.");
+                System.out.println(rowsInserted + " row(s) inserted in Books Table.");
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    private static void insertOperationAuthors(Connection connection) {
+    private static void insertOperationAuthors(Connection connection, int authorid) {
         try {
-            // Insert a new book
+
+
+            if(authorExists(connection, 0)) {
+                System.out.println("Author with ID " + authorid + " already exists.");
+                return;
+            }
+
+            // Insert new Authors
             String insertQuery = "INSERT INTO authors (authorid, firstname, lastname) " + 
             "VALUES (?,?,?)";
 
             try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
-                preparedStatement.setInt(1, 1);
+                preparedStatement.setInt(1, authorid);
                 preparedStatement.setString(2, "Gabrielle-Suzanne"); 
                 preparedStatement.setString(3, "Barbot de Villeneuve");
 
                 int rowsInserted = preparedStatement.executeUpdate();
-                System.out.println(rowsInserted + " row(s) inserted.");
+                System.out.println(rowsInserted + " row(s) inserted in Authors Table.");
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private static boolean authorExists(Connection connection, int authorid) throws SQLException {
+        // Check if the author with the given ID already exists
+        String checkAuthorQuery = "SELECT COUNT(*) FROM authors WHERE authorid = ?";
+        try (PreparedStatement checkAuthorStatement = connection.prepareStatement(checkAuthorQuery)) {
+            checkAuthorStatement.setInt(1, authorid);
+            try (ResultSet resultSet = checkAuthorStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    int count = resultSet.getInt(1);
+                    return count > 0;
+                }
+            }
+        }
+        return false;
     }
 
 
