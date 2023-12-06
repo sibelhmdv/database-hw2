7ccb6ea0217026619b7816a5fe3c886488a81e1f
sibelhmdv <78097592+sibelhmdv@users.noreply.github.com>
2023-12-06 17:36:42 +0400

    Update DatabaseConnection.java

--- bookstore/src/DatabaseConnection.java
+++ bookstore/src/DatabaseConnection.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 
 public class DatabaseConnection {
+
     public static void main(String[] args) {
         // Database connection parameters
         String url = "jdbc:postgresql://localhost:5432/postgresAS2";
